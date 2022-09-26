dbc82d6c794a72f93777aba989a9b464d3b42f5d
qui.nguyen <nguyenhtqui@gmail.com>
2022-09-26 08:46:05 +0200

    feat: WIP presentation section; rework responsive typography by rem unit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Jumbotron from './components/Jumbotron';
 import Contact from './components/Contact';
+import Presentation from './components/Presentation'
 
 const HomePage = () => {
     return (
@@ -8,6 +9,7 @@ const HomePage = () => {
         <Header></Header>
         <Jumbotron></Jumbotron>
         <Contact></Contact>
+        <Presentation></Presentation>
     </>)
 }
 
--- src/pages/components/Presentation.tsx
+++ src/pages/components/Presentation.tsx
@@ -1,5 +1,26 @@
+import urlQuoteOpen from '../../assets/images/quote-pink-open.png';
+import urlQuoteClose from '../../assets/images/quote-pink-close.png';
+
 const Presentation = () => {
-    
+    return (
+        <div className="wrapper">
+            <div className="presentation">
+                <div className="presentation__img"></div>
+
+                <div className="presentation__title">
+                    <div className="quote-icon"></div>
+                    <div className="text"><h2>Bonjour, je m'appelle Thanh Qui</h2></div>
+                    <div className="quote-icon"></div>
+                </div>
+
+                <div className="presentation__sub-title">
+                    <p>Je suis une jeune développeuse Full Stack web et mobile. Je suis capable de développer des applications web monopage et de celles mobiles en utilisant des technologies de la dernière génération. Curieuse, rigoureuse et polyvalente, je cherche une opportunité de stage ou d'emploi dans ce domaine informatique</p>
+                </div>
+
+                <div className="presentation__author">-Qui Nguyen Developpeuse Fullstack JS</div>
+            </div>
+        </div>
+    )
 }
 
 export default Presentation;
